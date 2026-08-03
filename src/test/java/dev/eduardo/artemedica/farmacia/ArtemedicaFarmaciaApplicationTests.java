d07f7f66d1c42a05e6d1b7cec7788e54f26b342c
GhostBarragan <irminb9@gmail.com>
2026-08-02 23:00:32 -0600

    load project

---
+++ src/test/java/dev/eduardo/artemedica/farmacia/ArtemedicaFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eduardo.artemedica.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtemedicaFarmaciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
